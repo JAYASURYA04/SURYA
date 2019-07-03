0b603447fa07bf1112c0bc86de617e9d52871bb6
JAYASURYA04 <52407528+JAYASURYA04@users.noreply.github.com>
2019-07-03 16:38:08 +0530

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,7 +1,7 @@
-ch = str(input())
+ch= str(input())
 
 if(ch=='A' or ch=='a' or ch=='E' or ch =='e' or ch=='I'
 or ch=='i' or ch=='O' or ch=='o' or ch=='U' or ch=='u'):
-  print(ch, " Vowel")
+  print(" Vowel")
 else:
-  print(ch, "Consonant")
+  print("Consonant")
